286ae1ad6f73b5107755395cd52260264cbd9d50
sybio <clement.guillemain@gmail.com>
2012-04-24 22:36:56 +0200

    Unit test structure

---
+++ tests/ImageWorkshopTest.php
@@ -0,0 +1,126 @@
+<?php
+require_once('../src/ImageWorkshop.php');
+ 
+class ImageWorkshopTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @todo
+     */
+    public function testPasteImage()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testAddLayer()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testMergeDown()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testMergeAll()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testMoveTop()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testMoveBottom()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testMoveTo()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testMoveUp()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testMoveDown()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testRemove()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testGetLayerLevel()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testIsLayerInIndex()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testGenerateImage()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testResizeByPixel()
+    {
+        $this->assertTrue(false);
+    }
+    
+    /**
+     * @todo
+     */
+    public function testResizeByPourcent()
+    {
+        $this->assertTrue(false);
+    }
+}
+?>
