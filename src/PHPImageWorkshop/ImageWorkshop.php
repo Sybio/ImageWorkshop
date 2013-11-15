c93479fe09e9537ae3aacae37f4430ae1a6ddf89
HEMC <hemendra.chaudhary619@gmail.com>
2013-11-15 12:22:21 +0530

    new method to load images by url.

--- src/PHPImageWorkshop/ImageWorkshop.php
+++ src/PHPImageWorkshop/ImageWorkshop.php
@@ -94,6 +94,29 @@ class ImageWorkshop
     }
     
     /**
+     * Initialize a layer from a given image url
+     * 
+     * 
+     * @param string $url
+     * 
+     * @return ImageWorkshopLayer
+     */
+    public static function initFromUrl($url) {
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_BINARYTRANSFER, 1);
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $image = self::initFromString($data);
+    }
+    /**
      * Initialize a text layer
      * 
      * @param string $text
