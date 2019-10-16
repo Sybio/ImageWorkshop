bf13ccb2895519aa56de743a2165556963e586b4
Jérémy DECOOL <contact@jdecool.fr>
2019-10-16 21:03:43 +0200

    Update autoloading to PSR-4

--- tests/autoload.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once(__DIR__.'/../src/PHPImageWorkshop/Exception/ImageWorkshopBaseException.php');
-require_once(__DIR__.'/../src/PHPImageWorkshop/Exception/ImageWorkshopException.php');
-require_once(__DIR__.'/../src/PHPImageWorkshop/Exif/ExifOrientations.php');
-require_once(__DIR__.'/../src/PHPImageWorkshop/Core/Exception/ImageWorkshopLayerException.php');
-require_once(__DIR__.'/../src/PHPImageWorkshop/Core/ImageWorkshopLib.php');
-require_once(__DIR__.'/../src/PHPImageWorkshop/Core/ImageWorkshopLayer.php');
-require_once(__DIR__.'/../src/PHPImageWorkshop/ImageWorkshop.php');
