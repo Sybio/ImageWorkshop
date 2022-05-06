195a4c4bda259fa35bb0ab3b50b8853a772a9285
Guilherme Saldanha <guisaldanha@gmail.com>
2022-05-06 15:49:48 -0300

    Fix in convertHexToRGB method. PHP 8.1 does not allow the input string to be null

--- src/Core/ImageWorkshopLib.php
+++ src/Core/ImageWorkshopLib.php
@@ -77,9 +77,9 @@ class ImageWorkshopLib
     public static function convertHexToRGB($hex)
     {
         return array(
-            'R' => (int) base_convert(substr($hex, 0, 2), 16, 10),
-            'G' => (int) base_convert(substr($hex, 2, 2), 16, 10),
-            'B' => (int) base_convert(substr($hex, 4, 2), 16, 10),
+            'R' => (int) base_convert(substr($hex ?? '', 0, 2), 16, 10),
+            'G' => (int) base_convert(substr($hex ?? '', 2, 2), 16, 10),
+            'B' => (int) base_convert(substr($hex ?? '', 4, 2), 16, 10),
         );
     }
 
